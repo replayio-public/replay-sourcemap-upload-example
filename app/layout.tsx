5505f8f56b2a09387f4e2442fbdd5e51871aa115
Mark Erikson <mark@isquaredsoftware.com>
2024-06-07 14:26:01 -0400

    Add docs link and drop Redux links

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,66 +17,14 @@ export default function RootLayout({ children }: Props) {
         <body>
           <section className={styles.container}>
             <Nav />
+            <main className={styles.main}>
+            
+            <h1>Replay Sourcemap Configuration Example</h1>
+            <h2>See the <a href="https://docs.replay.io/reference/replay-cli/source-maps" style={{color: "default"}}>Replay Sourcemap Upload docs</a></h2>
+              {children}
+              </main>
 
-            <header className={styles.header}>
-              <Image
-                src="/logo.svg"
-                className={styles.logo}
-                alt="logo"
-                width={100}
-                height={100}
-              />
-            </header>
-
-            <main className={styles.main}>{children}</main>
-
-            <footer className={styles.footer}>
-              <span>Learn </span>
-              <a
-                className={styles.link}
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://redux-toolkit.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Redux Toolkit
-              </a>
-              <span>, </span>
-              <a
-                className={styles.link}
-                href="https://react-redux.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                React Redux
-              </a>
-              ,<span> and </span>
-              <a
-                className={styles.link}
-                href="https://reselect.js.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Reselect
-              </a>
-            </footer>
+            
           </section>
         </body>
       </html>
